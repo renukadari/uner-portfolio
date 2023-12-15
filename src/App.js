7d78b2cd326380fd9ee3eb8caa9d06d7b8dc5ce8
renukadari <renukakadari27@gmail.com>
2023-12-15 21:47:01 +0530

    commit by renu kaddari27 email

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code>                        Happily live
+          Edit <code>src/App.js</code>                      eternally  Happily live
         </p>
         <a
           className="App-link"
